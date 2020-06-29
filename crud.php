672fe74ce5f73c3bcf46c32ce7d644830f5ddecf
Daniil Alekseev <dapcula08@yandex.ru>
2020-06-29 22:08:53 +0300

    crud added

---
+++ crud.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Yellowtail&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap-grid.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+   <link rel="stylesheet" type="text/css" href="css/user.css">
+</head>
+
+<body>
+
+   <nav class="navbar navbar-expand-md bg-info navbar-dark">
+      <!-- Brand -->
+      <a class="navbar-brand" href="index.html">Hedo Cakes</a>
+
+      <!-- Toggler/collapsibe Button -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Navbar links -->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+         <ul class="navbar-nav">
+            <li class="nav-item">
+               <a class="nav-link" href="aboutus.html">О нас</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="contact.html">Контакты</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="crud.php">Пользователи</a>
+            </li>
+         </ul>
+      </div>
+   </nav>
+   <?php require_once 'process.php'; ?>
+
+   <?php
+   if (isset($_SESSION['message'])) :
+   ?>
+
+      <div class="alert alert-<?= $_SESSION['msg_type'] ?>">
+         <?php
+         echo $_SESSION['message'];
+         unset($_SESSION['message']);
+         ?>
+      </div>
+   <?php endif ?>
+
+   <div class="container">
+      <?php
+      $mysqli = new mysqli('std-mysql', 'std_972', 'dapa0803', 'std_972') or die(mysqli_error($mysqli));
+      $result = $mysqli->query("SELECT * FROM data2") or die($mysqli->error);
+      //pre_r($result);
+      //pre_r($result->fetch_assoc());
+      ?>
+
+      <div class="row justify-content-center">
+         <table class="table">
+            <thead>
+               <tr>
+                  <th>Имя</th>
+                  <th>Почта</th>
+                  <th colspan="2">Действие</th>
+               </tr>
+            </thead>
+            <?php
+            while ($row = $result->fetch_assoc()) : ?>
+               <tr>
+                  <td><?php echo $row['name']; ?></td>
+                  <td><?php echo $row['location']; ?></td>
+                  <td>
+                     <a href="crud.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Редактировать</a>
+                     <a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Удалить</a>
+                  </td>
+               </tr>
+            <?php endwhile; ?>
+         </table>
+      </div>
+
+
+      <?php
+      function pre_r($array)
+      {
+         echo '
+   <pre>';
+         print_r($array);
+         echo '<pre>';
+      }
+
+      ?>
+
+      <div class=" row justify-content-center">
+         <form action="process.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <div class="form-group">
+               <label>Имя</label>
+               <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Введите ваше имя">
+            </div>
+            <div class="form-group">
+               <label>Почта</label>
+               <input type="text" name="location" class="form-control" value="<?php echo $location; ?>" placeholder="Введите вашу почту">
+            </div>
+            <div class="form-group">
+               <?php
+               if ($update == true) :
+               ?>
+                  <button class="btn btn-info" type="submit" name="update">Обновить</button>
+               <?php else : ?>
+                  <button class="btn btn-primary" type="submit" name="save">Сохранить</button>
+               <?php endif; ?>
+            </div>
+         </form>
+      </div>
+   </div>
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,57 @@
+<?php
+
+session_start();
+
+$mysqli = new mysqli('std-mysql', 'std_972', 'dapa0803', 'std_972') or die(mysqli_error($mysqli));
+
+$id = 0;
+$update = false;
+$name = '';
+$location = '';
+
+if (isset($_POST['save'])) {
+   $name = $_POST['name'];
+   $location = $_POST['location'];
+
+   $mysqli->query("INSERT INTO data2 (name, location) VALUES('$name','$location')") or die($mysqli->error);
+
+   $_SESSION['message'] = "Пользователь был добавлен!";
+   $_SESSION['msg_type'] = "success";
+
+   header("location: crud.php");
+}
+
+
+if (isset($_GET['delete'])) {
+   $id = $_GET['delete'];
+   $mysqli->query("DELETE FROM data2 WHERE id=$id") or die($mysqli->error);
+
+   $_SESSION['message'] = "Пользователь был удален!";
+   $_SESSION['msg_type'] = "danger";
+
+   header("location: crud.php");
+}
+
+if (isset($_GET['edit'])) {
+   $id = $_GET['edit'];
+   $update = true;
+   $result = $mysqli->query("SELECT * FROM data2 WHERE id=$id") or die($mysqli->error);
+   if (count($result) == 1) {
+      $row = $result->fetch_array();
+      $name = $row['name'];
+      $location = $row['location'];
+   }
+}
+
+if (isset($_POST['update'])) {
+   $id = $_POST['id'];
+   $name = $_POST['name'];
+   $location = $_POST['location'];
+
+   $mysqli->query("UPDATE data2 SET name='$name', location='$location' WHERE id=$id") or die($mysqli->error);
+
+   $_SESSION['message'] = "Информация о пользователе была обнолвена";
+   $_SESSION['msg_type'] = "warning";
+
+   header('location: crud.php');
+}
